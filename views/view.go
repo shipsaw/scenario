156257fd91bddcd2ed58c3c284340b611ee3f697
shipsaw <kirby.paulh@gmail.com>
2021-06-30 09:49:07 -0400

    Added bootstrap layout and integrated with other vies

--- views/view.go
+++ views/view.go
@@ -4,16 +4,23 @@ import (
 	"html/template"
 )
 
-func NewView(files ...string) *View {
-	files = append(files, "views/layouts/footer.gohtml")
+func NewView(layout string, files ...string) *View {
+	files = append(files,
+		"views/layouts/bootstrap.gohtml",
+		"views/layouts/footer.gohtml",
+	)
 
 	template, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
 	}
-	return &View{Template: template}
+	return &View{
+		Template: template,
+		Layout:   layout,
+	}
 }
 
 type View struct {
 	Template *template.Template
+	Layout   string
 }

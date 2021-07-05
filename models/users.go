b8f9d3673b62002716e651f117a4a4fe72b382ed
shipsaw <kirby.paulh@gmail.com>
2021-07-04 23:15:46 -0400

    Added UserService to handle specific DB interactions

--- models/users.go
+++ models/users.go
@@ -1,11 +1,56 @@
 package models
 
 import (
+	"errors"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+var (
+	ErrNotFound = errors.New("models: resource not found")
+)
+
+// Contains the specific type of DB that we are working with
+type UserService struct {
+	db *gorm.DB
+}
+
 type User struct {
 	gorm.Model
 	Email string `gorm:"not null;unique_index"`
+}
+
+func NewUserService(connectionInfo string) (*UserService, error) {
+	db, err := gorm.Open("postgres", connectionInfo)
+	if err != nil {
+		return nil, err
+	}
+	db.LogMode(true)
+	return &UserService{
+		db: db,
+	}, nil
+}
+
+func (us *UserService) Close() error {
+	return us.db.Close()
+}
+
+func (us *UserService) ByID(id uint) (*User, error) {
+	var user User
+	err := us.db.First(&user, "id=?", id).Error
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
+
+// Drops and rebuilds user table
+func (us *UserService) DestructiveReset() {
+	us.db.DropTableIfExists(&User{})
+	us.db.AutoMigrate(&User{})
 }

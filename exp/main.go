7fc1e299dd3069be6d5b8404143bbb0e3bde20a8
shipsaw <kirby.paulh@gmail.com>
2021-07-07 23:22:38 -0400

    Added cookie setting to login and signup actions

--- exp/main.go
+++ exp/main.go
@@ -2,50 +2,14 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/shipsaw/scenario/models"
+	"net/http"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "postgres"
-	dbname   = "postgres"
-)
-
-type User struct {
-	gorm.Model
-	Email string `gorm:"not null;unique_index"`
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello from the internet")
 }
 
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	us, err := models.NewUserService(psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer us.Close()
-	// us.DestructiveReset()
-	us.AutoMigrate()
-	user := models.User{
-		Email:    "michael@yahoo.com",
-		Password: "password",
-	}
-	if err := us.Create(&user); err != nil {
-		panic(err)
-	}
-
-	if err := us.Delete(user.ID); err != nil {
-		panic(err)
-	}
-
-	// userByID, err := us.ByID(user.ID)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	fmt.Println(user)
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe("localhost:3000", nil)
 }
--- main.go
+++ main.go
@@ -38,5 +38,6 @@ func main() {
 	router.HandleFunc("/signup", usersC.Create).Methods("POST")
 	router.Handle("/login", usersC.LoginView).Methods("GET")
 	router.HandleFunc("/login", usersC.Login).Methods("POST")
+	router.HandleFunc("/cookietest", usersC.CookieTest).Methods("GET")
 	http.ListenAndServe(":3000", router)
 }
